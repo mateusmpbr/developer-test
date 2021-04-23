9131b49911b547cc053955d5e5a25cbae19d2dd1
mateusmpbr <mateusmartinspereira@gmail.com>
2021-04-23 04:10:11 -0300

    Add Message controller and route

---
+++ src/controllers/MessageController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from "express"
+import {Message} from "../models/Message"
+
+export const index = async (Request: Request, Response: Response) => {
+    // TODO: implements logic
+}
+
+export const create = async (Request: Request, Response: Response) => {
+    // TODO: implements logic
+}
+
+export const store = async (Request: Request, Response: Response) => {
+    // TODO: implements logic
+}
---
+++ src/routes/Message.ts
@@ -0,0 +1,10 @@
+import {Router} from "express";
+import * as MessageController from "../controllers/MessageController"
+
+const router = Router();
+
+router.get('/messages', MessageController.index);
+router.get('/messages/create', MessageController.create);
+router.post('/messages', MessageController.store);
+
+export {router}
