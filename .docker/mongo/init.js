3f5cacb3efb005f9951a2995fda4f8a398a03988
mateusmpbr <mateusmartinspereira@gmail.com>
2021-04-24 01:44:20 -0300

    Add Docker

---
+++ .docker/mongo/init.js
@@ -0,0 +1,7 @@
+db.messages.insertMany([
+    {message: "Random Message One"},
+    {message: "Random Message Two"},
+    {message: "Random Message Three"},
+    {message: "Random Message Four"},
+    {message: "Random Message Five"},
+]);
