4106b8cd89fcf59157ca7e688bee642ef8f39606
mateusmpbr <mateusmartinspereira@gmail.com>
2021-04-25 16:03:35 -0300

    Add random medias to MongoDB

---
+++ .docker/mongo/2_init_medias.js
@@ -0,0 +1,7 @@
+db.medias.insertMany([
+    {media: "/images/Random-Media-One.jpg"},
+    {media: "/images/Random-Media-Two.jpg"},
+    {media: "/images/Random-Media-Three.jpg"},
+    {media: "/images/Random-Media-Four.jpg"},
+    {media: "/images/Random-Media-Five.jpg"},
+]);
