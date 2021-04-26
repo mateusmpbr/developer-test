feae5702e780cc217858c18e9afd464bf39d4d6d
mateusmpbr <mateusmartinspereira@gmail.com>
2021-04-26 00:45:34 -0300

    Divide config into sequelize.json & mongoose.json

--- src/database/Mongoose.ts
+++ src/database/Mongoose.ts
@@ -1,4 +1,4 @@
-import { mongoose as config } from '../config/development.json'
+import { development as config } from '../config/mongoose.json'
 const mongoose = require('mongoose')
 
 mongoose.connect(`mongodb://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}?authSource=admin`, { useNewUrlParser: true, useUnifiedTopology: true })
--- src/database/Sequelize.ts
+++ src/database/Sequelize.ts
@@ -1,4 +1,4 @@
-import { sequelize as config } from '../config/development.json'
+import { development as config } from '../config/sequelize.json'
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
