188ea4f850e154e22b8063521ec390c835016cd2
mateusmpbr <mateusmartinspereira@gmail.com>
2021-04-26 02:53:46 -0300

    Install and use cors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { serveStatic } from 'lasso/middleware'
 
 import errorTemplate from '@views/error.marko'
 
+var cors = require('cors')
 var createError = require('http-errors')
 var path = require('path')
 var cookieParser = require('cookie-parser')
@@ -25,6 +26,7 @@ var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
