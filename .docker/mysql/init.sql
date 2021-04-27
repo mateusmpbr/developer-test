f350a5a28a022b6ca76dc1cf9d8ef9e9247be5be
mateusmpbr <mateusmartinspereira@gmail.com>
2021-04-26 23:39:55 -0300

    Add seed to init.sql

--- .docker/mysql/init.sql
+++ .docker/mysql/init.sql
@@ -5,3 +5,12 @@ CREATE TABLE `Message` (
     `media` VARCHAR(255),
     `message` VARCHAR(255)
 )  ENGINE=INNODB;
+
+INSERT INTO
+    `Message`(`from`, `target_id`, `media`, `message`)
+VALUES
+    ('123','321','/images/Random-Media-One.jpg', 'Random Message One'),
+    ('456','123','/images/Random-Media-Two.jpg', 'Random Message Two'),
+    ('789','321','/images/Random-Media-Three.jpg', 'Random Message Three'),
+    ('321','456','/images/Random-Media-Four.jpg', 'Random Message Four'),
+    ('321','789','/images/Random-Media-Five.jpg', 'Random Message Five');
